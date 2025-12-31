1bbfc8f41cc7383daf262cbb20643c77860ad4ff
aditipokharel0 <paditi22@tbc.edu.np>
2025-12-31 21:17:06 +0545

    Add global flash messages component

--- resources/views/components/flash.blade.php
+++ resources/views/components/flash.blade.php
@@ -1,11 +1,11 @@
-@if(session('success'))
-    <div class="mb-4 p-3 border rounded bg-green-50">
+@if (session('success'))
+    <div class="mb-4 rounded border border-green-200 bg-green-50 px-4 py-3 text-green-800">
         {{ session('success') }}
     </div>
 @endif
 
-@if(session('error'))
-    <div class="mb-4 p-3 border rounded bg-red-50">
+@if (session('error'))
+    <div class="mb-4 rounded border border-red-200 bg-red-50 px-4 py-3 text-red-800">
         {{ session('error') }}
     </div>
 @endif
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,6 +32,8 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-6">
         @include('partials.flash')
     </div>
+    <x-flash />
+
     {{ $slot }}
 </main>
 
